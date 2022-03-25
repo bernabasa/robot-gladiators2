ffa6bddf96862cd8433590213c1bb313ece3c297
Your Name <bernabas.abdeta@yahoo.com>
2022-03-24 20:36:59 -0400

    merging

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 var playerName = window.prompt("What is your robot's name?");
 var playerHealth = 100;
 var playerAttack = 10;
@@ -42,3 +43,36 @@ var fight = function () {
     
 };
 fight();
+||||||| parent of 6bbc0f8 (second phase)
+window.alert("This is an alert! JavaScript running!");
+=======
+var playerName = window.prompt("What is your robot's name?");
+var playerHealth = 100;
+var playerAttack = 10;
+
+// You can also log multiple values at once like this
+console.log(playerName, playerAttack, playerHealth);
+
+
+var enemyName = "Roborto";
+var enemyHealth = 50;
+var enemyAttack = 12;
+
+var fight = function () {
+    window.alert("Welcome to robot Gladiators!");
+      //Subtract the value of `playerAttack` from the value of `enemyHealth` and use that result to update the value in the `enemyHealth` variable
+    enemyHealth = enemyHealth - playerAttack;
+  // Log a resulting message to the console so we know that it worked.
+    console.log(
+        playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining. "
+
+    );
+  // Subtract the value of `enemyAttack` from the value of `playerHealth` and use that result to update the value in the `playerHealth` variable.
+    playerHealth = playerHealth - enemyAttack;
+  // Log a resulting message to the console so we know that it worked.
+    console.log (
+        enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining. "
+    );
+};
+fight();
+>>>>>>> 6bbc0f8 (second phase)
